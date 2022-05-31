e03e7c914323da1466ee2d77166f6b13e0465a6c
mrrezki <mrezki@hotmail.com>
2022-05-31 12:20:24 +0800

    section 8

---
+++ backend/api/views.py
@@ -0,0 +1,17 @@
+from django.http import JsonResponse
+import json
+from products.models import Product
+
+
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
+        # model instcan (model_data)
+        # turn it into a pthon dict
+        # return JSON to my client
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,13 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api"
 
-get_response = requests.get(endpoint, json={"query": "Hellow world"})
+get_response = requests.get(
+    endpoint, params={"abc": 123}, json={"query": "Hellow world"}
+)
 
-print(get_response.text)
-get_response = requests.get(endpoint)
+# print(get_response.text)
 print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
