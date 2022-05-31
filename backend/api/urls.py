852c13c11bc02f98655361036ff73d46ff043619
mrrezki <mrezki@hotmail.com>
2022-05-31 15:09:21 +0800

    section 13

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views  # form .views import api_home
 
-urlpatterns = [path("", views.api_home)]  # localhost:8000/api/
+urlpatterns = [
+    path("", views.api_home),  # localhost:8000/api/
+    # path('products/', include('products.urls'),
+]
