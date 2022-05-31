027d1579d421246ba20c3ccaf3d99cc36ed4beab
mrrezki <mrezki@hotmail.com>
2022-05-31 15:41:45 +0800

    section 14

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,21 @@
 from itertools import product
+from math import prod
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
